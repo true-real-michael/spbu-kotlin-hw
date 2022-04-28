3c96aca59d1ee3b32d91c77adae440ba006b8ede
michael <kiselev.0353@gmail.com>
2022-04-28 14:29:42 +0300

    Initial commit, two versions, no unit-tests :D

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.19.0")
     testImplementation(platform("org.junit:junit-bom:5.8.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
---
+++ src/main/kotlin/homeworks/hw7/task2/main.kt
@@ -0,0 +1,88 @@
+package homeworks.hw7.task2
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.Job
+import kotlin.random.Random.Default.nextInt
+import kotlin.system.measureTimeMillis
+
+
+typealias Matrix = Array<IntArray>
+
+operator fun Matrix.times(other: Matrix): Matrix {
+    val r1 = this.size
+    val c1 = this[0].size
+    val r2 = other.size
+    val c2 = other[0].size
+    require(c1 == r2)
+    val result = Array(r1) { IntArray(c2) }
+    for (i in 0 until r1)
+        for (j in 0 until c2)
+            for (k in 0 until c1)
+                result[i][j] += this[i][k] * other[k][j]
+    return result
+}
+
+
+suspend fun multiplyMatrices(m1: Matrix, m2: Matrix): Matrix = coroutineScope {
+    val r1 = m1.size
+    val c1 = m1[0].size
+    val r2 = m2.size
+    val c2 = m2[0].size
+    require(c1 == r2)
+    val result = Array(r1) { IntArray(c2) }
+    val jobs = arrayOfNulls<Job>(r1)
+
+    for (i in 0 until r1)
+        jobs[i] = launch {
+            for (j in 0 until c2)
+                for (k in 0 until c1)
+                    result[i][j] += m1[i][k] * m2[k][j]
+        }
+    for (job in jobs)
+        job!!.join()
+
+    return@coroutineScope result
+}
+
+suspend fun multiplyMatrices1(m1: Matrix, m2: Matrix): Matrix = coroutineScope {
+    val r1 = m1.size
+    val c1 = m1[0].size
+    val r2 = m2.size
+    val c2 = m2[0].size
+    require(c1 == r2)
+    val result = Array(r1) { IntArray(c2) }
+    val jobs = arrayOfNulls<Job>(r1)
+
+    for (i in 0 until r1)
+        jobs[i] = launch {
+            val row = IntArray(c2)
+            for (j in 0 until c2)
+                for (k in 0 until c1)
+                    row[j] += m1[i][k] * m2[k][j]
+            result[i] = row
+        }
+    for (job in jobs)
+        job!!.join()
+
+    return@coroutineScope result
+}
+
+
+fun genMatrix(r: Int, c: Int): Matrix {
+    val result = Array(r) { IntArray(c) }
+    for (i in 0 until r)
+        for (j in 0 until c)
+            result[i][j] = nextInt()
+    return result
+}
+
+
+fun main(): Unit = runBlocking {
+    val m1 = genMatrix(1500, 1500)
+    val m2 = genMatrix(1500, 1500)
+    println(measureTimeMillis { m1 * m2 })
+    println(measureTimeMillis { multiplyMatrices(m1, m2) })
+    println(measureTimeMillis { multiplyMatrices1(m1, m2) })
+}
